46f89d1d99d5079970e06bd49c13516f95e473fe
Jawad Bish <Jawad.bish@gmail.com>
2021-09-21 01:25:38 +0300

    styles

--- client/src/components/Facts/Fact/Fact.js
+++ client/src/components/Facts/Fact/Fact.js
@@ -1,47 +1,49 @@
 import React from 'react'
 import useStyles from './styles';
-import { Card, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import topCardImg from '../../../images/bbb.png';
 
-const Fact = ( { fact, setCurrentId } ) => {
+const Fact = ({ fact, setCurrentId }) => {
     const styleclass = useStyles();
     return (
-    <Card className={styleclass.card}>
-        {/* <CardMedia className={styleclass.media} title={fact.title}/> */}
-        <CardMedia className={styleclass.media} image={topCardImg} title={fact.title}/>
-        <div className={styleclass.overlay}>
-            <Typography variant="h6">{fact.creator}</Typography>
-            <Typography variant="body2">{moment(fact.createdAt).fromNow()}</Typography>
-        </div>
-        <div className={styleclass.overlay2}>
-            <Button style={{color: 'white'}} 
-            size="small"
-            onClick={() => setCurrentId(fact._id)}>
-            <MoreHorizIcon fontSize="default"/>
-            </Button>
+        <Card className={styleclass.card}>
+            {/* <CardMedia className={styleclass.media} title={fact.title}/> */}
+            <CardMedia className={styleclass.media} image={topCardImg} title={fact.title} />
+            <div className={styleclass.overlay}>
+                <Typography variant="h6">{fact.creator}</Typography>
+                <Typography variant="body2">{moment(fact.createdAt).fromNow()}</Typography>
+            </div>
+            <div className={styleclass.overlay2}>
+                <Button style={{ color: 'white' }}
+                    size="small"
+                    onClick={() => setCurrentId(fact._id)}>
+                    <MoreHorizIcon fontSize="default" />
+                </Button>
+                <Typography className={styleclass.category} variant="h6" gutterButtom>{fact.category}</Typography>
             </div>
             <div className={styleclass.details}>
-            <Typography variant="body2" color="textSecondary">{fact.tags.map((tag) => `#${tag} ` )}</Typography>
+                <Typography className={styleclass.message} variant="body5" gutterButtom>{fact.message}</Typography>
+
             </div>
             <CardContent>
-            <Typography  className={styleclass.title} variant="h5" gutterButtom>{fact.message}</Typography>
+                <Typography variant="body2" color="textSecondary">{fact.tags.map((tag) => `#${tag} `)}</Typography>
             </CardContent>
             <CardActions className={styleclass.cardActions}>
-        <Button size="small" color="primary" onClick={()=>{}}>
-          <ThumbUpAltIcon fontSize="small" />
-         Like
-         {fact.likeCount}
-        </Button>
-        <Button size="small" color="primary" onClick={()=> {}}>
-          <DeleteIcon fontSize="small" />
-         Delete
-        </Button>
+                <Button size="small" color="primary" onClick={() => { }}>
+                    <ThumbUpAltIcon fontSize="small" />
+                    Like
+                    {fact.likeCount}
+                </Button>
+                <Button size="small" color="primary" onClick={() => { }}>
+                    <DeleteIcon fontSize="small" />
+                    Delete
+                </Button>
             </CardActions>
-    </Card>
+        </Card>
     )
 }
 
--- client/src/components/Facts/Fact/styles.js
+++ client/src/components/Facts/Fact/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles({
   media: {
     height: 0,
-    paddingTop: '56.25%',
+    paddingTop: '30.25%',
     backgroundColor: '#990000',
     backgroundBlendMode: 'darken',
   },
