a1673547a8f4988ff65d4a3da98f19f2b2db7ae5
Jawad Bish <Jawad.bish@gmail.com>
2021-09-22 00:10:47 +0300

    environment variables

--- server/index.js
+++ server/index.js
@@ -2,23 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 
 import factsRoutes from './routes/facts.js';
 
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/facts', factsRoutes);
 
 //mongoDB cloud\atlas
-const CONNECTION_URL = 'mongodb+srv://jawadbisharat:Nazareth2020@cluster0.wq5f9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://jawadbisharat:Nazareth2020@cluster0.wq5f9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() =>
         app.listen(PORT, () => console.log(`SERVER running on port: ${PORT}`)))
     .catch((err) => console.log(err));
