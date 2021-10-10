00a25f8269f2081e128ad6e03ab8c590dbebc02c
Jawad Bish <Jawad.bish@gmail.com>
2021-10-10 23:34:43 +0300

    Deployment - BE and FE

--- server/index.js
+++ server/index.js
@@ -16,15 +16,15 @@ app.use(cors());
 app.use('/facts', factsRoutes);
 app.use('/users', userRoutes);
 
-// app.use('/', (req, res) => {
-//     res.send('Welcome to Facts App')
-// });
+app.get('/', (req, res) => {
+    res.send('Hello There!');
+});
 
+const PORT1 = (process.env.PORT || 5000);
+const CONNECTION_URL = 'mongodb+srv://jawadbisharat:Nazareth2020@cluster0.wq5f9.mongodb.net/FactsDatabase?retryWrites=true&w=majority';
 
-const PORT = process.env.PORT || 5000;
-
-
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() =>
-        app.listen(PORT, () => console.log(`SERVER running on port: ${PORT}`)))
+        app.listen(PORT1, () => console.log(`SERVER running on port: ${PORT1}`)))
     .catch((err) => console.log(err));
+
